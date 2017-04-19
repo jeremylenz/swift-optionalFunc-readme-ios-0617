b21963d81cd4959a95b17adeb546528463a1df37
jeremylenz <jeremylenz@gmail.com>
2017-04-19 10:44:18 -0400

    names

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -46,5 +46,5 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
     return name
 }
 
-produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
+produceFullName(withFirstName: "David", middleName: "Jeremy", lastName: "Lenz")
 produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
